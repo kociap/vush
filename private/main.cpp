53e11331423b3fd145d10c188256e9f969c68ecd
Piotr Kocia <p.kocia@wp.pl>
2020-08-02 08:51:42 +0200

    moving to cmake's FetchContent

--- private/main.cpp
+++ private/main.cpp
@@ -1,15 +1,22 @@
 #include <vush/vush.hpp>
 
+#include <chrono>
 #include <iostream>
 
 int main() {
     anton::Array<anton::String> import_directories{anton::variadic_construct, anton::String{u8"C:/Users/an0num0us/Documents/vush2/build/shaders"}};
-    anton::Array<vush::Constant_Define> constant_defines{anton::variadic_construct, vush::Constant_Define{anton::String{u8"_HAS_TEXTURE"}, 1}};
+    anton::Array<vush::Constant_Define> constant_defines{anton::variadic_construct, vush::Constant_Define{anton::String{u8"_HAS_TEXTURE"}, 1},
+                                                         vush::Constant_Define{anton::String{u8"_CASTS_SHADOWS"}, 1},
+                                                         vush::Constant_Define{anton::String{u8"_EMISSIVE"}, 1}};
     vush::Configuration config;
     config.defines = anton::move(constant_defines);
     config.import_directories = anton::move(import_directories);
-    config.source_name = u8"C:/Users/an0num0us/documents/vush2/build/shaders/shader.vush";
+    config.source_name = u8"C:/Users/an0num0us/documents/vush2/build/shaders/skin.vush";
+    auto t1 = std::chrono::high_resolution_clock::now();
     anton::Expected<vush::Build_Result, anton::String> result = vush ::compile_to_glsl(config);
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+    std::cout << "execution time " << duration << "ns\n";
     if(result) {
         vush::Build_Result const& res = result.value();
         for(vush::Pass_Settings const& pass_settings: res.settings) {
