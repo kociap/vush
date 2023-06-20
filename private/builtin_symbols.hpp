50ea21b48c71b992380bac5defa1cecb35504b92
Piotr Kocia <peter.kocia@gmail.com>
2023-06-21 01:31:39 +0200

    add autogenerated get_builtin_type

--- private/builtin_symbols.hpp
+++ private/builtin_symbols.hpp
@@ -2,8 +2,13 @@
 
 #include <anton/slice.hpp>
 
-#include <ast_fwd.hpp>
+#include <ast2.hpp>
 
 namespace vush {
     [[nodiscard]] anton::Slice<ast::Decl_Overloaded_Function const* const> get_builtin_functions_declarations();
-}
+
+    // get_builtin_type
+    // Get an immutable builtin type.
+    //
+    [[nodiscard]] ast::Type_Builtin const* get_builtin_type(ast::GLSL_Type type);
+} // namespace vush
