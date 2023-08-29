47f40e3e01ebe5643bc214f3f34d10b702e48ab8
Piotr Kocia <peter.kocia@gmail.com>
2023-08-29 18:50:20 +0200

    extract Scoped_Symbol_Table as Scoped_Map

---
+++ compiler/vush_core/scoped_map.hpp
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <anton/flat_hash_map.hpp>
+
+#include <vush_core/types.hpp>
+
+namespace vush {
+  template<typename Key, typename Value>
+  struct Scoped_Map {
+  private:
+    using Entry_Map = anton::Flat_Hash_Map<Key, Value>;
+
+    Array<Entry_Map> scopes;
+    Allocator* allocator;
+
+  public:
+    Scoped_Map(Allocator* allocator): scopes(allocator), allocator(allocator)
+    {
+      // Push global scope.
+      push_scope();
+    }
+
+    // find_entry
+    // Looks up an entry with the given key in the scopes starting from the innermost and
+    // progressing towards the outermost.
+    //
+    // Returns:
+    // Pointer to the value of the entry or nullptr if not found.
+    // Adding a new entry to the scope might invalidate the pointer.
+    //
+    [[nodiscard]] Value const* find_entry(Key const& name) const
+    {
+      for(i64 i = scopes.size() - 1; i >= 0; --i) {
+        Entry_Map const& map = scopes[i];
+        auto result = map.find(name);
+        if(result != map.end()) {
+          return &result->value;
+        }
+      }
+      return nullptr;
+    }
+
+    // add_entry
+    // Adds an entry to the current scope.
+    // Adding an entry might invalidate pointers previously returned by find_entry.
+    //
+    void add_entry(Key const& key, Value const& value)
+    {
+      Entry_Map& map = scopes.back();
+      map.emplace(key, value);
+    }
+
+    // push_scope
+    // Add a new scope.
+    //
+    void push_scope()
+    {
+      scopes.emplace_back(allocator);
+    }
+
+    // pop_scope
+    // Pops the current scope. Does not pop the global scope.
+    //
+    void pop_scope()
+    {
+      if(scopes.size() > 1) {
+        scopes.pop_back();
+      }
+    }
+  };
+} // namespace vush
